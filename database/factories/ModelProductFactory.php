2df7536c1c5b33063e225f434386ec957e9ff587
dana <saddthathsara@gmail.com>
2019-11-26 12:23:18 +0530

    seed data to products and reviews

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->word,
+        'details'=>$faker->paragraph,
+        'price'=>$faker->numberBetween(100,1000),
+        'discount'=>$faker->numberBetween(5,20),
+        'stock'=>$faker->randomDigit
     ];
 });
